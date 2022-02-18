5e28acdd00ff25c39155051a596c69af0d85fc84
Monica Giambitto <m.giambitto@freegoweb.it>
2022-02-18 22:18:59 +0100

    extract reading input to function

--- chapter_1/go/tip_calculator.go
+++ chapter_1/go/tip_calculator.go
@@ -16,17 +16,13 @@ import (
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin) // Read from stdin
+
 	fmt.Print("Enter the bill amount: ")
-	reader := bufio.NewReader(os.Stdin)     // Read from stdin
-	input, _ := reader.ReadString('\n')     // ReadString will block until the delimiter is entered
-	input = strings.TrimSuffix(input, "\n") // remove the delimeter from the string
-	bill, _ := strconv.ParseFloat(input, 64)
+	bill := input_to_float(reader)
 
 	fmt.Print("Enter the rate: ")
-	reader = bufio.NewReader(os.Stdin)      // Read from stdin
-	input, _ = reader.ReadString('\n')      // ReadString will block until the delimiter is entered
-	input = strings.TrimSuffix(input, "\n") // remove the delimeter from the string
-	rate, _ := strconv.ParseFloat(input, 64)
+	rate := input_to_float(reader)
 
 	tip := calculateTip(bill, rate)
 	total := bill + tip
@@ -37,4 +33,12 @@ func main() {
 
 func calculateTip(bill float64, rate float64) float64 {
 	return math.Round(bill / 100 * rate)
+}
+
+func input_to_float(reader *bufio.Reader) float64 {
+	input, _ := reader.ReadString('\n')     // ReadString will block until the delimiter is entered
+	input = strings.TrimSuffix(input, "\n") // remove the delimeter from the string
+	input_in_float, _ := strconv.ParseFloat(input, 64)
+
+	return input_in_float
 }
