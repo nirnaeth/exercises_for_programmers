a71afe6e70a6ebdfbcae4133c2cbb220f0badb6a
Monica Giambitto <m.giambitto@freegoweb.it>
2022-02-25 23:36:36 +0100

    repeat questions until correct amount format is entered

--- chapter_1/go/tip_calculator.go
+++ chapter_1/go/tip_calculator.go
@@ -20,34 +20,56 @@ import (
 func main() {
 	reader := bufio.NewReader(os.Stdin) // Read from stdin
 
-	fmt.Print("Enter the bill amount: ")
-	input, _ := reader.ReadString('\n') // ReadString will block until the delimiter is entered
-	bill := InputToFloat(input)
+	var bill, rate float64
 
-	fmt.Print("Enter the rate: ")
-	input, _ = reader.ReadString('\n') // ReadString will block until the delimiter is entered
-	rate := InputToFloat(input)
+	bill = retrieveBill(reader, bill)
+	rate = retrieveRate(reader, rate)
 
 	tip := CalculateTip(bill, rate)
 	total := bill + tip
 
 	fmt.Println("The tip is ", tip)
-	fmt.Println("The total is ", total)
+	fmt.Println("The total bill is ", total)
+}
+
+func retrieveRate(reader *bufio.Reader, rate float64) float64 {
+	for {
+		fmt.Print("Enter the rate: ")
+		input, _ := reader.ReadString('\n') // ReadString will block until the delimiter is entered
+		value, err := InputToFloat(input)
+
+		if err == nil {
+			rate = value
+			break
+		}
+	}
+
+	return rate
+}
+
+func retrieveBill(reader *bufio.Reader, bill float64) float64 {
+	for {
+		fmt.Print("Enter the bill amount: ")
+
+		input, _ := reader.ReadString('\n') // ReadString will block until the delimiter is entered
+		value, err := InputToFloat(input)
+
+		if err == nil {
+			bill = value
+			break
+		}
+	}
+	return bill
 }
 
 func CalculateTip(bill float64, rate float64) float64 {
 	return math.Round(bill / 100 * rate)
 }
 
-func InputToFloat(input string) float64 {
+func InputToFloat(input string) (float64, error) {
 	input = strings.TrimSuffix(input, "\n") // remove the delimeter from the string
 
 	converted, err := strconv.ParseFloat(input, 64)
 
-	if err != nil {
-		fmt.Printf("Amount entered '%s' is not a number\n", input)
-		os.Exit(1)
-	}
-
-	return converted
+	return converted, err
 }
--- chapter_1/go/tip_calculator_test.go
+++ chapter_1/go/tip_calculator_test.go
@@ -22,38 +22,38 @@ func TestCalculateTip(t *testing.T) {
 }
 
 // Code from https://stackoverflow.com/questions/26225513/how-to-test-os-exit-scenarios-in-go
-func TestExitWithErrorWhenInputIsNotANumber(t *testing.T) {
-	invalid_bill := "not a number"
+// func TestExitWithErrorWhenInputIsNotANumber(t *testing.T) {
+// 	invalid_bill := "not a number"
 
-	if os.Getenv("CRASH") == "1" { // setting env to test the wrongful exit status
-		output := captureOutput(func() { // capturing output to verify message, see support function at the bottom
-			InputToFloat(invalid_bill)
-		})
+// 	if os.Getenv("CRASH") == "1" { // setting env to test the wrongful exit status
+// 		output := captureOutput(func() { // capturing output to verify message, see support function at the bottom
+// 			InputToFloat(invalid_bill)
+// 		})
 
-		expected_message := "Amount entered 'not a number' is not a number\n"
+// 		expected_message := "Amount entered 'not a number' is not a number\n"
 
-		if output != expected_message { // this is actually not working
-			t.Errorf("got %s, expected %s", output, expected_message)
-		}
-		return
-	}
+// 		if output != expected_message { // this is actually not working
+// 			t.Errorf("got %s, expected %s", output, expected_message)
+// 		}
+// 		return
+// 	}
 
-	cmd := exec.Command(os.Args[0], "-test.run=TestExitWithErrorWhenInputIsNotANumber")
-	cmd.Env = append(os.Environ(), "CRASH=1")
-	err := cmd.Run()
-	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
-		return
-	}
+// 	cmd := exec.Command(os.Args[0], "-test.run=TestExitWithErrorWhenInputIsNotANumber")
+// 	cmd.Env = append(os.Environ(), "CRASH=1")
+// 	err := cmd.Run()
+// 	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+// 		return
+// 	}
 
-	t.Fatalf("process ran with err %v, want exit status 1", err)
-}
+// 	t.Fatalf("process ran with err %v, want exit status 1", err)
+// }
 
 func TestNumberToFloat(t *testing.T) {
 	input := "100"
 
 	expected := 100.0
 
-	number := InputToFloat(input)
+	number, _ := InputToFloat(input)
 
 	if number != expected {
 		t.Errorf("got %f, expected %f", number, expected)
@@ -62,10 +62,10 @@ func TestNumberToFloat(t *testing.T) {
 
 // Support
 // captures the output to stdout to test the messages
-func captureOutput(f func()) string {
-	var buf bytes.Buffer
-	log.SetOutput(&buf)
-	f()
-	log.SetOutput(os.Stderr)
-	return buf.String()
-}
+// func captureOutput(f func()) string {
+// 	var buf bytes.Buffer
+// 	log.SetOutput(&buf)
+// 	f()
+// 	log.SetOutput(os.Stderr)
+// 	return buf.String()
+// }
